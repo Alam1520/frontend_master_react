cc97a3c8ad0822a62a52bf5887c1435b01bd4c2c
Husnan Alam <alam@memoriki.com>
2022-06-24 12:19:32 +0800

    eslint change
    added search component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
-};
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Adopt Me"),
-        React.createElement(Pet, {
-            name: "Fido",
-            animal: "Dog",
-            breed: "Labrador",
-        }),
-        React.createElement(Pet, {
-            name: "Bella",
-            animal: "Cat",
-            breed: "Persian",
-        }),
-        React.createElement(Pet, { name: "Max", animal: "Dog", breed: "Labrador" })
-    );
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
