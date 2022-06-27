086fa5a54ad0d3ffbb251e6a686e523a66b9b81e
Husnan Alam <alam@memoriki.com>
2022-06-27 09:43:07 +0800

    small changes

--- src/Details.js
+++ src/Details.js
@@ -3,53 +3,68 @@ import { Component } from "react";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
-    state = { loading: true };
+  state = { loading: true, showModal: false };
 
-    async componentDidMount() {
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
-        );
-        const json = await res.json();
-        this.setState({ loading: false, ...json.pets[0] });
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading ...</h2>;
     }
 
-    render() {
-        if (this.state.loading) {
-            return <h2>loading ...</h2>;
-        }
+    const { animal, breed, city, state, description, name, images, showModal } =
+      this.state;
 
-        const { animal, breed, city, state, description, name, images } =
-            this.state;
-
-        return (
-            <div className="details">
-                <Carousel images={images} />
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button onClick={this.toggleModal} style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
+          <p>{description}</p>
+          {
+            showModal ? (
+              <Modal>
                 <div>
-                    <h1>{name}</h1>
-                    <h2>
-                        {animal} - {breed} - {city}, {state}
-                    </h2>
-                    <ThemeContext.Consumer>
-                        {([theme]) => (
-                            <button style={{ backgroundColor: theme }}>Adopt {name}</button>
-                        )}
-                    </ThemeContext.Consumer>
-                    <p>{description}</p>
+                  <h1>Would you like to adopt {name}?</h1>
+                  <div className="buttons">
+                    <a href="https://bit.ly/pet-adopt">Yes!</a>
+                    <button onClick={this.toggleModal}>No!!</button>
+                  </div>
                 </div>
-            </div>
-        );
-    }
+              </Modal>
+            ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 const WrappedDetails = () => {
-    const params = useParams();
-    return (
-        <ErrorBoundary>
-            <Details params={params} />
-        </ErrorBoundary>
-    );
+  const params = useParams();
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 
 export default WrappedDetails;
--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -2,40 +2,40 @@ import { Component } from "react";
 import { Link, Navigate } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-    state = {
-        hasError: false,
-        redirect: false
+  state = {
+    hasError: false,
+    redirect: false,
+  };
+
+  static getDerivedStateFromError() {
+    return {
+      hasError: true,
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => {
+        this.setState({ hasError: true });
+      }, 5000);
+    }
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Navigate to="/" />;
+    }
+    if (this.state.hasError) {
+      return (
+        <h2>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to back to the home page or wait five seconds and we will do it for you.
+        </h2>
+      );
     }
 
-    static getDerivedStateFromError() {
-        return {
-            hasError: true
-        };
-    }
-
-    componentDidUpdate() {
-        if (this.state.hasError) {
-            setTimeout(() => {
-                this.setState({ hasError: true });
-            }, 5000);
-        }
-    }
-
-    render() {
-        if (this.state.redirect) {
-            return <Navigate to="/" />
-        }
-        if (this.state.hasError) {
-            return (
-                <h2>
-                    There was an error with this listing. <Link to="/">Click here</Link>{" "}
-                    to back to the home page or wait five seconds.
-                </h2>
-            );
-        }
-
-        return this.props.children;
-    }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
